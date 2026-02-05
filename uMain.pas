327f4e0c69024986b32277c8373469a3341eeef0
Shaun Roselt <Me@ShaunRoselt.com>
2026-02-05 21:29:52 +0200

    Are we there yet?

---
+++ uMain.pas
@@ -0,0 +1,25 @@
+unit uMain;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms3D, FMX.Types3D, FMX.Forms, FMX.Graphics, 
+  FMX.Dialogs;
+
+type
+  TfrmMain = class(TForm3D)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+{$R *.fmx}
+
+end.
